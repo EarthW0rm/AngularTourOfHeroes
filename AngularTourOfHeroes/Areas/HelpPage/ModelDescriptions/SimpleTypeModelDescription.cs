54b5064d22e26e813ed80167f0b6ee9af3767a0b
Marco Faustino <marco.a.faustino@gmail.com>
2018-01-19 17:39:47 -0200

    TECHNIQUES > Visual Studio 2015 QuickStart > Step 2: Create the Visual Studio ASP.NET project

---
+++ AngularTourOfHeroes/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularTourOfHeroes.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AngularTourOfHeroes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularTourOfHeroes.WebApiApplication" Language="C#" %>
