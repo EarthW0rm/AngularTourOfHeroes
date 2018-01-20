67862d466a31fc3ce0054371059342a4e164d9bd
Marco Faustino <marco.a.faustino@gmail.com>
2018-01-20 14:25:37 -0200

    TUTORIAL > 3. The Hero Editor > Edit the hero, Two-way binding, The missing FormsModule

--- AngularTourOfHeroes/src/app/app.module.ts
+++ AngularTourOfHeroes/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, HeroesComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
