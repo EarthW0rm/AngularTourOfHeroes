9c723ada302f30402d622f3c09db525ec4c70f25
Marco Faustino <marco.a.faustino@gmail.com>
2018-01-19 22:25:32 -0200

    TUTORIAL > 2. The Application Shell > Change the application title, Add application styles

--- AngularTourOfHeroes/src/app/app.component.ts
+++ AngularTourOfHeroes/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = 'Angular Build Via F5'; }
+export class AppComponent  {
+    title = 'Tour of Heroes';
+}
