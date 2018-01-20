cc654e6c47abecaf3c94ebf401cf045e917bc9ba
Marco Faustino <marco.a.faustino@gmail.com>
2018-01-20 17:00:27 -0200

    TUTORIAL > 6. Services > Create MessagesComponent, Create the MessageService, Inject it into the HeroService, Send a message from HeroService, Bind to the MessageService

--- AngularTourOfHeroes/src/app/app.module.ts
+++ AngularTourOfHeroes/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { MessagesComponent } from './messages/messages.component';
+
 import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
     bootstrap: [AppComponent],
-    providers: [HeroService]
+    providers: [HeroService, MessageService]
 })
 export class AppModule { }
